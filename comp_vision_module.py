8d1cf92f5a78e724006e8e1204fbfed972511380
tomwillcode <67656514+tomwillcode@users.noreply.github.com>
2024-05-06 19:08:39 -0400

    Comp_vision module activated!
    wooo!

---
+++ comp_vision_module.py
@@ -0,0 +1,38 @@
+from deepface import DeepFace
+import numpy as np
+
+models = [
+  "VGG-Face",
+  "Facenet",
+  "Facenet512",
+  "OpenFace",
+  "DeepFace",
+  "DeepID",
+  "ArcFace",
+  "Dlib",
+  "SFace",
+]
+
+backends = [
+  'opencv', 
+  'ssd', 
+  'dlib', 
+  'mtcnn', 
+  'fastmtcnn',
+  'retinaface', 
+  'mediapipe',
+  'yolov8',
+  'yunet',
+  'centerface',
+]
+
+
+def compare_faces(img1,img2):
+  result_list = []
+  for i in backends:
+    try:
+      result = DeepFace.verify(img1_path=img1, img2_path=img2, detector_backend = i)
+      result_list.append(result['distance'])
+    except Exception:
+      pass
+  return np.mean(result_list)
